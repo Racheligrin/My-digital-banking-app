c3338361989a3f804ff72afbdef6d9656297f6fc
Racheligrin <r0583262119@gmail.com>
2026-06-10 02:11:35 +0300

    .
    .

--- backend/src/main/java/com/example/banking/repository/TransactionRepository.java
+++ backend/src/main/java/com/example/banking/repository/TransactionRepository.java
@@ -4,5 +4,4 @@ import com.example.banking.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-    // יורש את כל הפונקציות הבסיסיות (שמירה, מחיקה ושליפה) מ-JpaRepository
 }
