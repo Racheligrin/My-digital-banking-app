6365c65fad4044d218feb7a3ad6827358af64607
Racheligrin <r0583262119@gmail.com>
2026-06-10 02:08:44 +0300

    .
    .

--- backend/src/main/java/com/example/banking/repository/AccountRepository.java
+++ backend/src/main/java/com/example/banking/repository/AccountRepository.java
@@ -5,6 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
-    // פונקציה מותאמת אישית שתעזור לנו למצוא חשבון לפי מספר החשבון שלו (ולא לפי ה-ID)
     Optional<Account> findByAccountNumber(String accountNumber);
 }
