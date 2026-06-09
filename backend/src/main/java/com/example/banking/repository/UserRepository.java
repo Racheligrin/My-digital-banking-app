c4f452f37872abec1c240cf5630b941377db8fbe
Racheligrin <r0583262119@gmail.com>
2026-06-10 02:11:52 +0300

    .
    .

--- backend/src/main/java/com/example/banking/repository/UserRepository.java
+++ backend/src/main/java/com/example/banking/repository/UserRepository.java
@@ -5,6 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    // מאפשר לנו למצוא משתמש לפי שם המשתמש שלו (מעולה להתחברות בעתיד)
     Optional<User> findByUsername(String username);
 }
